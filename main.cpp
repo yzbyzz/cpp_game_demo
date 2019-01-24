94ae01523c35cc1124593f8424221cc9e05c4efd
qingzhi <qingzhi@hoosho.com>
2019-01-24 17:59:17 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    Sleep(1000);
+    system("cls");
+    cout << "===Hello world!" << endl;
+    return 0;
+}
